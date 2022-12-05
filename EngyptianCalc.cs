135ceecfa5ba430043a1aeaeb067e5ffa86f7795
Sbonga Mabaso <mabasos@wavesoft.co.za>
2022-12-05 13:31:33 +0200

    modifie to fewer lines

--- EngyptianCalc.cs
+++ EngyptianCalc.cs
@@ -9,24 +9,15 @@ namespace EgyptianCalculator
 {
     class EngyptianCalc
     {
-        public class pair
-        {
-            public int first, second;
-            public pair(int first, int second)
-            {
-                this.first = first;
-                this.second = second;
-            }
-        }
         // - Public function to get first & second number
         public static int[] getTwoNumbers()
         {
-            Console.WriteLine("Enter Two numbers to Multiply. \n");
+            Console.WriteLine("Enter Two numbers to Multiply:");
 
-            Console.WriteLine("Multiplicand: \n");
+            Console.WriteLine("Multiplicand :");
             int multiplicand = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Multiplier: \n");
+            Console.WriteLine("Multiplier   :");
             int multiplier = Convert.ToInt32(Console.ReadLine());
 
             int[] numbers = new int[] { multiplicand, multiplier };
@@ -34,76 +25,7 @@ namespace EgyptianCalculator
             return numbers;
         }
 
-        private static void MultiplicationByDoubling(int multiplicand, int multiplier)
-        {
-            Console.WriteLine("\n---| MultiplicationByDoubling |---");
-
-            int value = 1;
-            var firstColumn = new ArrayList();
-            //Add powers of two to the first column (multiplicand)
-            while(value <= multiplicand)
-            {
-                firstColumn.Add(value);
-                value = 2 * value;
-            }
-
-            Console.WriteLine("First Column:\n");
-            for(int i = 0; i<firstColumn.Count; i++)
-            {
-                Console.WriteLine(firstColumn[i] + ", ");
-            }
-            
-
-            //Create array for the second column
-            var secondColumn = new ArrayList();
-            for (int i=0; i<firstColumn.Count; i++)
-            {
-                secondColumn.Add(multiplier);
-                multiplier = 2 * multiplier;
-            }
-            Console.WriteLine("First Column:\n");
-            for (int i = 0; i < secondColumn.Count; i++)
-            {
-                Console.WriteLine(secondColumn[i] + ", ");
-            }
-
-            //Find elements that add up to a Multiplicand (firstColumn) and store in a hash table
-            Dictionary<int, pair> map = new Dictionary<int, pair>();
-            for(int i=0; i<firstColumn.Count; i++)
-            {
-                for(int j=i+1; j<firstColumn.Count; j++)
-                {
-                    if (map.ContainsKey((int)firstColumn[i] + (int)firstColumn[j]))
-                    {
-                        map[(int)firstColumn[i] + (int)firstColumn[j]] = new pair(i, j);
-                    }
-                    else
-                    {
-                        map.Add((int)firstColumn[i] + (int)firstColumn[j], new pair(i, j));
-                    }
-                }
-            }
-            //Traverse through all the pairs and search for multiplicand
-            for(int i=0; i<firstColumn.Count; i++)
-            {
-                for (int j=i+1; j<firstColumn.Count; j++)
-                {
-                    int sum = (int)firstColumn[i] + (int)firstColumn[j];
-                    //if multiplicand is present in the hash table
-                    if(map.ContainsKey(multiplicand - sum))
-                    {
-                        pair p = map[multiplicand - sum];
-                        if(p.first != i && p.first != j && p.second != i && p.second != j)
-                        {
-                            Console.WriteLine(firstColumn[i] + ", " + firstColumn[j] + ", " + firstColumn[p.first] + ", " + firstColumn[p.second]);
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-
-        private static int MultiplicationByHalving(int multiplicand, int multiplier)
+        private static int EgyptianMultiplication(int multiplicand, int multiplier)
         {
             int product;
 
@@ -113,11 +35,11 @@ namespace EgyptianCalculator
             }
             else if (multiplicand % 2 != 0)
             {
-                product = multiplier + MultiplicationByHalving(multiplicand / 2, 2 * multiplier);
+                product = multiplier + EgyptianMultiplication(multiplicand / 2, 2 * multiplier);
             }
             else //if(multiplicand % 2 == 0)
             {
-                product = MultiplicationByHalving(multiplicand / 2, 2 * multiplier);
+                product = EgyptianMultiplication(multiplicand / 2, 2 * multiplier);
             }
 
             int product_temp = 0;
@@ -125,38 +47,22 @@ namespace EgyptianCalculator
             {
                 product_temp = product;
             }
-            Console.WriteLine("\nSteps: {0} -> {1}", multiplicand, multiplier);
-            Console.WriteLine("\nProduct: {0}", product_temp);
+            Console.WriteLine("\n->|  {0} --> {1}  |  {2}  |", multiplicand, multiplier, product_temp);
             return product;
         }
 
         // - Driver Function
         static void Main()
         {
-            Console.WriteLine("Select Multiplication Type: \n-------------------- \n | 1 - Doubling. \n | 2 - Halving. \n | 3 - Addtion. \n--------------------");
-
-            int multiplicationType = Convert.ToInt32(Console.ReadLine());
-
+            Console.WriteLine("------------------------------");
             int[] numbers = getTwoNumbers();
 
             // Numbers to multiply.
             int multiplicand = numbers[0];
             int multiplier = numbers[1];
 
-            if (multiplicationType == 1)
-            {
-                MultiplicationByDoubling(multiplicand, multiplier);
-            }
-            else if(multiplicationType == 2)
-            {
-                MultiplicationByHalving(multiplicand, multiplier);
-            }
-            else
-            {
-                Console.WriteLine("Not Applicable");
-            }
-
+            Console.WriteLine("\n-----------------------------");
+            EgyptianMultiplication(multiplicand, multiplier);
         }
-
     }
 }
